e30837272b2f06a2c264e8613c2bb1c171809a42
nochet <ngomae@missouri.edu>
2018-08-27 15:23:53 -0500

    directory for short protocol

---
+++ base_pop/scripts/assembly_shortProtocol/stringt_short_abund.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+#  SBATCH CONFIG
+#-------------------------------------------------------------------------------
+## resources
+#SBATCH --partition Lewis
+#SBATCH --nodes=1
+#SBATCH --ntasks=1  # used for MP#SBATCH -e error_%A_%a.err # Standard errorI codes, otherwise leav$
+##SBATCH --cpus-per-task=12  # cores per task
+#SBATCH --mem-per-cpu=8G  # memory per core (default is 1GB/core)
+#SBATCH --array=1-54
+#SBATCH --time 0-02:00  # days-hours:minutes
+#SBATCH --qos=normal
+#SBATCH --account=kinglab  # investors will replace this with their account name
+#
+## labels and outputs
+#SBATCH --job-name=S06_abundance_enoch
+#
+#SBATCH -o abundance_%A_%a.out # Standard output
+#SBATCH -e error_%A_%a.err # Standard error
+
+
+## notifications
+#SBATCH --mail-user=ngomae@missouri.edu  # email address for notifications
+#SBATCH --mail-type=END,FAIL  # which type of notifications to send
+#-------------------------------------------------------------------------------
+
+echo "### Starting at: $(date) ###"
+
+module load stringtie/stringtie-1.3.3b
+
+COMMANDA=`head -n ${SLURM_ARRAY_TASK_ID} S03_abundances_short.txt | tail -n 1`
+eval $COMMANDA
+
+echo "### Ending at: $(date) ###"
