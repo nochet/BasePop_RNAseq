d454b6ac0cdf852d7271784c844341b3308eee39
nochet <ngomae@missouri.edu>
2018-11-02 17:06:15 -0500

    PC3 and 4 in DESeq_viz.Rmd

---
+++ base_pop/scripts/DESeq_scripts/modified_plotPCA_function.R
@@ -0,0 +1,29 @@
+# plotPCA function modified to extract PC3 and PC4
+
+pcaPlot <- function (object, intgroup = "condition", ntop = 500, returnData = FALSE) 
+{
+  rv <- rowVars(assay(object))
+  select <- order(rv, decreasing = TRUE)[seq_len(min(ntop, 
+                                                     length(rv)))]
+  pca <- prcomp(t(assay(object)[select, ]))
+  percentVar <- pca$sdev^2/sum(pca$sdev^2)
+  if (!all(intgroup %in% names(colData(object)))) {
+    stop("the argument 'intgroup' should specify columns of colData(dds)")
+  }
+  intgroup.df <- as.data.frame(colData(object)[, intgroup, 
+                                               drop = FALSE])
+  group <- if (length(intgroup) > 1) {
+    factor(apply(intgroup.df, 1, paste, collapse = " : "))
+  }
+  else {
+    colData(object)[[intgroup]]
+  }
+  d <- data.frame(PC1 = pca$x[, 1], PC2 = pca$x[, 2], PC3 = pca$x[, 3], PC4 = pca$x[, 4], group = group, 
+                  intgroup.df, name = colnames(object))
+  if (returnData) {
+    attr(d, "percentVar") <- percentVar[1:2]
+    return(d)
+  }
+}
+
+
