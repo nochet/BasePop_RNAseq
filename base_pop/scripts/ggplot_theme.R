511cffd759df618d07ae7a34171fd144180f4f68
nochet <ngomae@missouri.edu>
2018-11-02 09:55:19 -0500

    minor updates to scripts

---
+++ base_pop/scripts/ggplot_theme.R
@@ -0,0 +1,8 @@
+
+font_size <- 10
+my_theme <- theme(
+  axis.text = element_text(size = font_size),
+  axis.title = element_text(size = font_size),
+  legend.text = element_text(size = font_size),
+  legend.title=element_text(size = font_size),
+  plot.title = element_text(size = font_size + 1))
