b0de8fcd3941f843b031443bf52722214f5b8089
nochet <ngomae@missouri.edu>
2018-12-13 16:49:13 -0600

    minor things

---
+++ base_pop/scripts/DESeq_scripts/t20DEGs.R
@@ -0,0 +1,15 @@
+FBgn0035022 <- grep("FBgn0035022", gsum$summary)
+gsum[FBgn0035022[1],'summary']
+pull(gsum[FBgn0035022[1],'summary'])
+
+FBgn0039677 <- grep("FBgn0039677", gsum$summary)
+gsum[FBgn0039677[1],'summary']
+pull(gsum[FBgn0039677[1],'summary'])
+
+FBgn0000639 <- grep("FBgn0000639", gsum$summary)
+gsum[FBgn0000639[1],'summary']
+pull(gsum[FBgn0000639[1],'summary'])
+
+FBgn0026563 <- grep("FBgn0026563", gsum$summary)
+gsum[FBgn0026563[1],'summary']
+pull(gsum[FBgn0026563[1],'summary'])
